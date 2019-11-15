cd738fb01ed89730022283dc1c6bbd416ca39fe4
p.danylchenko <p.danylchenko@levi9.com>
2019-11-15 14:17:58 +0200

    types added

--- steps/steps.ts
+++ steps/steps.ts
@@ -6,21 +6,33 @@ const basePage = new (require('../pageObjects/BasePage'))();
 const productionPage = new (require('../pageObjects/LoggedInPage'))();
 const repository = new (require('../pageObjects/RepositoryPage'))();
 
-Given('I open web-page', () => basePage.open());
-When('I enter e-mail {string} and password {string}', async (email, pass) => await helper.login(email, pass));
-Then('I see username {string} on the page', async username => {
-	const profileName = await productionPage.successLoginCheck(username);
-	expect(profileName).to.equal(username);
-});
+Given('I open web-page', (): Promise<void> => basePage.open());
+When(
+	'I enter e-mail {string} and password {string}',
+	async (email: string, pass: string): Promise<void> => await helper.login(email, pass),
+);
+Then(
+	'I see username {string} on the page',
+	async (username: string): Promise<void> => {
+		const profileName = await productionPage.successLoginCheck(username);
+		expect(profileName).to.equal(username);
+	},
+);
 
-When('I create new repository {string}', async repoName => repository.create(repoName));
-Then('I check readme.md file was created', async () => {
-	const readmePresence = await repository.readmeFilePresence();
-	expect(readmePresence).to.be.true;
-});
+When('I create new repository {string}', async (repoName: string): Promise<void> => repository.create(repoName));
+Then(
+	'I check readme.md file was created',
+	async (): Promise<void> => {
+		const readmePresence = await repository.readmeFilePresence();
+		expect(readmePresence).to.be.true;
+	},
+);
 
-When('I delete repository {string}', async repo => productionPage.deleteRepository(repo));
-Then('I check repository {string} was deleted', async repo => {
-	const repoPresence = await productionPage.checkRepositoryDeletion(repo);
-	expect(repoPresence).to.be.false;
-});
+When('I delete repository {string}', async (repo: string): Promise<void> => productionPage.deleteRepository(repo));
+Then(
+	'I check repository {string} was deleted',
+	async (repo: string): Promise<void> => {
+		const repoPresence = await productionPage.checkRepositoryDeletion(repo);
+		expect(repoPresence).to.be.false;
+	},
+);
